aec699515383284f23aa9333821ec2f6009ace53
harish-rzt <harish.v@razorthink.net>
2016-06-15 20:32:41 +0530

    scroll fix and animation changes

--- static-pages/script/script.js
+++ static-pages/script/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
+
     $('#feedContainer').scroll(function () {
+
         var scrollTop = $(this).scrollTop();
-        var opacityVal = 1 - scrollTop * 0.001;
-        var scaleVal = 1.2 - scrollTop * 0.001;
-        var topVal = - scrollTop;
-        $('#feedBanner').css({
-            'opacity': opacityVal,
-            'top': topVal,
-        });
+
         $('#feedBannerImage').css({
-            'transform': 'skewX(4.5deg) scale(' + scaleVal +')'
+            'transform': 'skewX(4.5deg) scale(' + (1.5 - scrollTop * 0.001) +')',
+            '-webkit-filter': 'blur(' + (scrollTop * 0.03) + 'px)'
         });
+
     });
+
 });
